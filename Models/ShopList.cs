bfcaf3d11fd8fd005d2b01a747a7bdb7673ebfc3
Cristina Roman <cristinamariaroman31@gmail.com>
2024-12-14 23:04:38 +0200

    Lab10 final

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -10,11 +10,14 @@ namespace Roman_Cristina_Lab7.Models
 {
     public class ShopList
     {
-         
+
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
     }
 }
