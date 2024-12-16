80294d09c78b53b112ee4fe6460354e631861813
Cristina Roman <cristinamariaroman31@gmail.com>
2024-12-16 18:57:11 +0200

    Lab10 final

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -19,8 +19,35 @@ namespace Roman_Cristina_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Products>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
+        }
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
         }
 
+        public Task<int> DeleteShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.DeleteAsync(shop);
+            }
+            else
+            {
+                return Task.FromResult(0);
+            }
+        }
         public Task<int> SaveProductAsync(Products product)
         {
             if (product.ID != 0)
